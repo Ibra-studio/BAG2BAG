0164ec626461d8120e1ddc06fdce200416d270b1
UIBRAHIM <135786335+seyboss@users.noreply.github.com>
2025-07-01 17:22:48 +0100

    feat: Ajout de la dépendance @vercel/speed-insights et intégration dans main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import React from "react";
 import { ModalProvider } from "./context/ModalContext.jsx";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 // @ts-ignore
 createRoot(document.getElementById("root")).render(
   <StrictMode>
