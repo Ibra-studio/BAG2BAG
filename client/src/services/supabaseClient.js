f23de9d57a0f528a97148128612678476d609056
UIBRAHIM <135786335+seyboss@users.noreply.github.com>
2025-07-01 16:58:55 +0100

    feat: Mise à jour de la configuration pour utiliser les variables d'environnement Supabase et ajout du fichier vite-env.d.ts

--- client/src/services/supabaseClient.js
+++ client/src/services/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sdhcwdviclpgsuiiyakg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkaGN3ZHZpY2xwZ3N1aWl5YWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MTE2NDAsImV4cCI6MjA2MDA4NzY0MH0.LuslalHAUuqeF08k1OEYdWrQ0OTQHKU8jU2znTAMkJo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+console.log(import.meta.env.VITE_SUPABASE_KEY);
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
